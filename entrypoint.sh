893b187d0894f4a3675027f327b412a3ebea5464
chougule007 <sachin.chougule@tango340b.com>
2022-03-30 17:23:52 +0530

    update action and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,23 +6,10 @@ set -e
 [ -z $INPUT_AWS_REGION ] && INPUT_AWS_REGION="us-east-1"
 [ -z $INPUT_RESOURCE_TYPE ] && INPUT_RESOURCE_TYPE="FILE"
 
-if [[ -z "$INPUT_AWS_ACCESS_KEY_ID" || \
-      -z "$INPUT_AWS_SECRET_ACCESS_KEY" ]]; then
-  echo "Please provide AWS credentials."
-  exit 1
-fi
-
 if [[ -z "$INPUT_SOURCE_PATH" ]]; then
   echo "Please provide local dist file."
   exit 1
 fi
-
-aws configure --profile upload-artifacts-profile <<-EOF > /dev/null 2>&1
-${INPUT_AWS_ACCESS_KEY_ID}
-${INPUT_AWS_SECRET_ACCESS_KEY}
-${INPUT_AWS_REGION}
-text
-EOF
 
 BRANCH_NAME=$(echo $GITHUB_REF | cut -d '/' -f 3- | tr / -)
 REPO_NAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f 2)
@@ -101,10 +88,3 @@ sh -c "aws s3 cp ${INPUT_SOURCE_PATH} s3://${INPUT_AWS_S3_BUCKET_NAME}/${DESTINA
         --profile upload-artifacts-profile \
         --no-progress \
         --metadata $METADATA $ARGS"
-
-aws configure --profile upload-artifacts-profile <<-EOF > /dev/null 2>&1
-null
-null
-null
-text
-EOF
